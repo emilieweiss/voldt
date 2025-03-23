61adb7889252965708b3ead86acd3a27f1a91bec
emilieweiss <emilieweiss@outlook.com>
2025-03-23 19:43:57 +0100

    Opsætning af fonts

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:voldt/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static _border([Color color = AppPallete.woltBlue]) => OutlineInputBorder(
-    borderSide: BorderSide(color: color, width: 1.2),
-    borderRadius: BorderRadius.circular(10),
-  );
+  static _border([Color color = AppPallete.woltBlue]) =>
+      OutlineInputBorder(
+        borderSide: BorderSide(color: color, width: 1.2),
+        borderRadius: BorderRadius.circular(10),
+      );
 
   static final lightThemeMode = ThemeData.light().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
     primaryColor: AppPallete.woltBlue,
-    appBarTheme: const AppBarTheme(backgroundColor: AppPallete.backgroundColor),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppPallete.backgroundColor,
+    ),
+    textTheme: ThemeData.light().textTheme.apply(
+      fontFamily: 'Omnes', // Set the font family here
+    ),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(20),
       enabledBorder: _border(),
